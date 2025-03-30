c22f4a84efc28a9a3ece9ac8749cac5a6dd93ae9
margulis <wfptny@gmail.com>
2025-03-31 00:09:05 +0200

    its working

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import org.example.hw22tymeleaf.ThymeleafServlet;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        ThymeleafServlet thymeleafServlet = new ThymeleafServlet();
+
+    }
+}
